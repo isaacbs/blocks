b4116a1b35bf15b7e5b44c2b79e6b30f485cc9b6
Peter <o.peter612@gmail.com>
2021-10-02 17:44:12 -0500

    Peter committing to work on it

---
+++ main.py
@@ -0,0 +1,14 @@
+
+## Step 1: Randomly choose how many retangles will be in a picture
+
+## Step 2: Generate initial ls
+
+
+## Step 2: Iterate over chosen number 
+
+        ## Step 4: Create Grid object
+
+
+
+
+
